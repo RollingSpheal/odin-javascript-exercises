31ea49813fd9e6c2180244ad5ffe91f121473870
RollingSpheal <191359688+RollingSpheal@users.noreply.github.com>
2025-01-08 10:00:33 +0700

    Complete javascript exercises 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let text = "";
+    if(num < 0) {
+        text = 'ERROR';
+    } else {
+        for(let i = num; i; i--) {
+            text = text.concat(string);
+        }
+    }
+    
+    return text;
 };
 
 // Do not edit below this line
