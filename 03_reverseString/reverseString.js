9b6e2731c2f718665b4425a37bbfbfbb9ea1d068
RollingSpheal <191359688+RollingSpheal@users.noreply.github.com>
2025-01-08 18:39:03 +0700

    Complete javascript exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let textReverse = "";
+    for(let length = text.length; length; length--) {
+        textReverse = textReverse.concat(text.at(length - 1));
+    }
+    return textReverse;
 };
 
 // Do not edit below this line
