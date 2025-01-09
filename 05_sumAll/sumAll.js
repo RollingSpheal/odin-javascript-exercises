280a245e5d1c28f321dc93df23544d4b57f6d6c6
RollingSpheal <191359688+RollingSpheal@users.noreply.github.com>
2025-01-09 18:05:33 +0700

    Complete javascript exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if (!Number.isInteger(x) || !Number.isInteger(y)) {
+        return "ERROR";
+    };
+    if (x < 0 || y < 0) {
+        return "ERROR";
+    ;}
+    if (y < x) {
+        let yDuplicate = y;
+        y = x;
+        x = yDuplicate;
+    }
+    let sum = 0;
+    for (let i = x; i <= y; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
