09ee8e4bf3c8a46880768f10af76b56c131183be
RollingSpheal <191359688+RollingSpheal@users.noreply.github.com>
2025-01-11 11:58:03 +0700

    Complete javascript exercise 10

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+    const alphanumerical = "abcdefghijklmnopqrstuvwxyz1234567890";
 
+    const newPhrase = phrase
+    .toLowerCase().
+    split('').
+    filter((characters) => alphanumerical.includes(characters))
+    .join('');
+
+    const reversedPhrase = newPhrase
+    .split('')
+    .reverse()
+    .join('');
+
+    return newPhrase === reversedPhrase;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    if (input < 0) return "OOPS";
 
+    if (input == 0) return 0;
+    
+    let fNum = 0;
+    let num1 = 0;
+    let num2 = 1;
+    for (let i = 1; i <= input; i++) {
+        num1 = num2;
+        num2 = fNum;
+        fNum = num1 + num2;
+    }
+    return fNum;
 };
 
 // Do not edit below this line
